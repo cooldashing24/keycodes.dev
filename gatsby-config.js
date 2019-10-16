7c495e718ba2feea545ab66516d3084211bb9d4f
Justin Juno <hello@justinjuno.dev>
2019-10-16 16:02:29 -0500

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Keycodes`,
+    description: `A developer resource for getting keyboard key codes. Key codes are numeric values that correspond to physical keys on the keyboard. With JavaScript, we can target these key codes and add events to them in our applications.`,
+    author: `@justinjunodev`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -21,14 +21,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,13 @@
 import React from "react"
-
+import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="404: Page Not found" />
+    <p>You are on the 404 page.</p>
+    <p><Link to="/">Go back home.</Link></p>
   </Layout>
 )
 
