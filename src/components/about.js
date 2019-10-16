c8c77b66a548cbcbf636c0b2a6f02e58e05c5010
Justin Juno <hello@justinjuno.dev>
2019-10-16 16:52:15 -0500

    replace utils with base styles and import fonts

--- src/components/about.js
+++ src/components/about.js
@@ -3,7 +3,8 @@ import React from 'react'
 const About = () => {
     return (
         <div className="layout--about">
-            about
+            <h1>Keycodes</h1>
+            <p>lorem ipsum dolar si amet.</p>
         </div>
     )
 }
